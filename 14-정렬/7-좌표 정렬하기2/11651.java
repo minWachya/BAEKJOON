f9cf32f67460e24fa667e0a906594d1ca62d08ef
minWachya <61674991+minWachya@users.noreply.github.com>
2020-08-28 14:36:35 +0900

    Create 11651.java

---
+++ 14-정렬/7-좌표 정렬하기2/11651.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        List<Point> point = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        
+        //입력
+        int x, y;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            point.add(new Point(x, y));
+        }
+        br.close();
+        
+        //정렬
+        Collections.sort(point);
+       
+        //출력
+        for (Point p : point)
+            bw.write(p + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
+
+class Point implements Comparable<Point> {
+    int x, y;
+    
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public int compareTo(Point p){
+        //y값이 같을 때 this.x가 크면 1 리턴, p.x가 크면 -1, x값도 같으면 0 리턴
+        if (this.y == p.y) return this.x > p.x ? 1 : (this.x < p.x ? -1 : 0);
+        //this.y가 크면 1 리턴, p.y가 크면 -1 리턴
+        else return this.y > p.y ? 1 : -1;
+    }
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+    
+}
